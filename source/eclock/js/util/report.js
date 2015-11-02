9b80c6a0ab5a8562c3595f3de8cf0ea00403a486
郭朋飞 <guopengfei@pengfei.local>
2015-11-02 20:06:19 +0800

    add report

---
+++ source/eclock/js/util/report.js
@@ -0,0 +1,28 @@
+var APPID = 1313260295;
+
+/*
+* @method get uuid
+* localStorage 没有则生成一个uuid
+* */
+var getUuid = function () {
+
+    var uuid = localStorage && localStorage.getItem('eclock_uuid');
+
+    if(uuid) {
+        return uuid;
+    }
+
+    return 'eclock' + new Date().getTime() + Math.round(Math.random() * Math.pow(10, 10));
+};
+
+/*
+* @method 上报mdata 和 GA
+* @param {String} eventName 事件名称
+* */
+module.exports = function (eventName) {
+    mData.push(['send', eventName, {
+        appid: APPID,
+        uuid : getUuid()
+    }]);
+    ga('send', 'UA-69564068-1', eventName);
+};
